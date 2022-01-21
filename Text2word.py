4d82ef64cb25c96ff5f7293c4c9697b7bc33fcd4
KISHORE <92573263+kishore67@users.noreply.github.com>
2022-01-21 21:57:30 +0530

    Update Text2word.py
    Read the text file and write in ms word using automation

--- Text2word.py
+++ Text2word.py
@@ -1,9 +1,18 @@
 import pyautogui as p
 import time as t
 
-msg1 = open("kishore1.txt", "r") # reading the text file
+msg1 = open("kishore1.txt", "r") # read the text file
 msg = msg1.read()
 t.sleep(5)
 
+p.press('win') # press windows key
+t.sleep(1)
+p.write('msword') #search msword
+t.sleep(2)
+p.press('enter')
+
+t.sleep(2)
+p.press("enter")
+
 for i in msg:
-    p.write(i) # writing the text file
+    p.write(i) # write the readed text file content in the ms word
